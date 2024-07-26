6882ec3e6b559167a03a241432b8af47ce8d08b1
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-07-26 12:49:47 +0900

    Update topdf.rb

---
+++ topdf.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Topdf < Formula
+  desc "a PDF converter tool."
+  homepage "https://github.com/crcrworks/topdf"
+  url "https://github.com/crcrworks/topdf/releases/download/v0.1.0/topdf"
+  sha256 "9fc3f104f1aa4ce2fa36a031513e46811dfd6756ff64cf8e7a1cec2970715efc"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "topdf"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test topdf`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
